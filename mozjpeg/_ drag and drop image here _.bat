d8ffaaeebfa65c57dfc18c64b0d32d7f46254b50
Sheep-y <your.sheepy@gmail.com>
2017-12-25 20:35:20 +0800

    Add non-jpeg conversion using ffmpeg.

--- mozjpeg/_ drag and drop image here _.bat
+++ mozjpeg/_ drag and drop image here _.bat
@@ -9,34 +9,50 @@ pause
 goto end
 
 :loop
-if /i not "%~x1"==".jpg" goto skip
-set "input=%~nx1"
+echo off
+set input=%~nx1
+if /i "%~x1"==".jpg" goto comp
+echo Converting %~nx1 to jpeg.
+set output=%~n1.jpg
+ffmpeg -y -loglevel error -i "%input%" -c:v mjpeg -frames:v 1 -q:v 2 -b:v 999999999k -an "%output%"
+if ERRORLEVEL 1 echo ERROR - Cannot convert %~nx1 to jpeg.
+if ERRORLEVEL 255 goto noffmpeg
+set input=%output%
+if exist "%output%" goto comp
+echo ERROR - Please check that the file is an image and source folder is writeable.
+echo.
+pause
+goto end
+
+:comp
 echo | set /p print=Compressing %~nx1 .
-set output==%input:.jpg=.q30.jpg%
+set output=%input:.jpg=.q30.jpg%
 "%~dp0cjpeg.exe" -quality 30 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q40.jpg%
+set output=%input:.jpg=.q40.jpg%
 "%~dp0cjpeg.exe" -quality 40 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q50.jpg%
+set output=%input:.jpg=.q50.jpg%
 "%~dp0cjpeg.exe" -quality 50 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q60.jpg%
+set output=%input:.jpg=.q60.jpg%
 "%~dp0cjpeg.exe" -quality 60 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q70.jpg%
+set output=%input:.jpg=.q70.jpg%
 "%~dp0cjpeg.exe" -quality 70 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q80.jpg%
+set output=%input:.jpg=.q80.jpg%
 "%~dp0cjpeg.exe" -quality 80 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q90.jpg%
+set output=%input:.jpg=.q90.jpg%
 "%~dp0cjpeg.exe" -quality 90 "%input%" > "%output%"
 echo | set /p print= done.
 goto next
 
-:skip
-echo %1 is not jpg.
+:noffmpeg
+echo ERROR - ffmpeg not found.  Please make sure ffmpeg is on the PATH environment variable.
+pause
+goto end
 
 :next
 echo.
--- mozjpeg/_ high quality _.bat
+++ mozjpeg/_ high quality _.bat
@@ -9,31 +9,47 @@ pause
 goto end
 
 :loop
-if /i not "%~x1"==".jpg" goto skip
-set "input=%~nx1"
+echo off
+set input=%~nx1
+if /i "%~x1"==".jpg" goto comp
+echo Converting %~nx1 to jpeg.
+set output=%~n1.jpg
+ffmpeg -y -loglevel error -i "%input%" -c:v mjpeg -frames:v 1 -q:v 2 -b:v 999999999k -an "%output%"
+if ERRORLEVEL 1 echo ERROR - Cannot convert %~nx1 to jpeg.
+if ERRORLEVEL 255 goto noffmpeg
+set input=%output%
+if exist "%output%" goto comp
+echo ERROR - Please check that the file is an image and source folder is writeable.
+echo.
+pause
+goto end
+
+:comp
 echo | set /p print=Compressing %~nx1 .
-set output==%input:.jpg=.q90.jpg%
+set output=%input:.jpg=.q90.jpg%
 "%~dp0cjpeg.exe" -quality 90 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q92.jpg%
+set output=%input:.jpg=.q92.jpg%
 "%~dp0cjpeg.exe" -quality 92 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q94.jpg%
+set output=%input:.jpg=.q94.jpg%
 "%~dp0cjpeg.exe" -quality 94 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q96.jpg%
+set output=%input:.jpg=.q96.jpg%
 "%~dp0cjpeg.exe" -quality 96 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q98.jpg%
+set output=%input:.jpg=.q98.jpg%
 "%~dp0cjpeg.exe" -quality 98 "%input%" > "%output%"
 echo | set /p print=.
-set output==%input:.jpg=.q100.jpg%
+set output=%input:.jpg=.q100.jpg%
 "%~dp0cjpeg.exe" -quality 100 "%input%" > "%output%"
 echo | set /p print= done.
 goto next
 
-:skip
-echo %1 is not jpg.
+:noffmpeg
+echo ERROR - ffmpeg not found.  Please make sure ffmpeg is on the PATH environment variable.
+pause
+goto end
 
 :next
 echo.
