5582cd03038e6137e82d92d49518f0ce3bf541d9
Sheep-y <your.sheepy@gmail.com>
2019-06-26 08:47:26 +0800

    Add faststart to h264 formats for quicker playing and youtube upload.

--- ffmpeg/cv_h264_crf16_vslow.bat
+++ ffmpeg/cv_h264_crf16_vslow.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-ffmpeg -y -hide_banner -i %1 -c copy -c:v libx264 -preset veryslow -crf 16 "%~n1.crf16%~x1"
+ffmpeg -y -hide_banner -i %1 -movflags +faststart -c copy -c:v libx264 -preset veryslow -crf 16 "%~n1.crf16%~x1"
 if %ERRORLEVEL% GEQ 1 pause
 shift
 if "%~1" neq "" goto loop
--- ffmpeg/cv_h264_nvenc_fast.bat
+++ ffmpeg/cv_h264_nvenc_fast.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-ffmpeg -y -hide_banner -i %1 -c copy -c:v h264_nvenc -preset fast "%~n1.n264%~x1"
+ffmpeg -y -hide_banner -i %1 -movflags faststart -c copy -c:v h264_nvenc -preset fast "%~n1.n264%~x1"
 if %ERRORLEVEL% GEQ 1 pause
 shift
 if "%~1" neq "" goto loop
